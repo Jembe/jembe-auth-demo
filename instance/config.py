42b063e3278551e23c3fcf71486cd3bfca85c8d1
Predrag Peranović <predrag.peranovic@blokkod.me>
2021-04-21 15:02:23 +0200

    support local session storage

--- instance/config.py
+++ instance/config.py
@@ -1,6 +1,14 @@
+import os
+
+instance_path = os.environ.get(
+    "FLASK_INSTANCE_PATH", os.path.join(os.getcwd(), "instance")
+)
+
 SQLALCHEMY_DATABASE_URI = "sqlite:///../data/jembe_auth_demo.sqlite"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 JEMBE_UPLOAD_FOLDER = "../data/media"
 # CSRF_COOKIE_PATH = ""
 # APPLICATION_ROOT = ""
-SECRET_KEY = b'\xeb\x13\xa0=\x04\xa5i!\xafd\x96\xc5\x07\xf0,k1\n\xfc\xf64\x08C\xe8'
+SECRET_KEY = b"\xeb\x13\xa0=\x04\xa5i!\xafd\x96\xc5\x07\xf0,k1\n\xfc\xf64\x08C\xe8"
+SESSION_TYPE = "filesystem"
+SESSION_FILE_DIR = os.path.join(instance_path, "../data/flask_session")
--- jembe_auth_demo/jmb.py
+++ jembe_auth_demo/jmb.py
@@ -1,12 +1,16 @@
 from flask_seasurf import SeaSurf 
+from flask_session import Session
 from jembe import Jembe
 
-__all__ = ("jmb", "csrf", "init_jembe")
+
+__all__ = ("jmb", "init_jembe")
 
 jmb = Jembe()
 csrf = SeaSurf()
+sess = Session()
 
 
 def init_jembe(app):
     csrf.init_app(app)
+    sess.init_app(app)
     jmb.init_app(app)
